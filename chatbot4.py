17ff9f2cf3e1b174e384f0e151946e8a66309d43
Jiahui Yang <brumaire1111@outlook.com>
2023-12-03 14:17:53 -0500

    Update chatbot4.py
    UI update

--- chatbot4.py
+++ chatbot4.py
@@ -9,7 +9,7 @@ from langchain.chat_models import ChatOpenAI
 import streamlit as st
 import time
 
-openai.api_key = "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"
+openai.api_key = "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"
 
 chat_model = ChatOpenAI(model="gpt-3.5-turbo-16k", temperature=0.7, openai_api_key=openai.api_key)
 
@@ -41,7 +41,7 @@ def ask_gpt3(question, documents):
     )
     return response.choices[0].message['content']
 
-def display_response(response):
+"""def display_response(response):
     container = st.empty()
     display_text = ""
     for char in response:
@@ -49,19 +49,56 @@ def display_response(response):
         container.markdown(display_text)
         time.sleep(0.05)  # 调整这个值以改变显示速度
     time.sleep(2)
-    #container.empty()
+    #container.empty()"""
     
+chat_history = []
+
+# 其他导入...
+
+def display_response(new_response):
+    global chat_history
+    chat_history.append(new_response)
+    container = st.empty()
+
+    # 逐字显示新回复
+    display_text = ""
+    for char in new_response:
+        display_text += char
+        # 使用 Markdown 格式显示对话
+        container.markdown("".join(chat_history[:-1] + [display_text]), unsafe_allow_html=True)
+        time.sleep(0.01)  # 调整这个值以改变显示速度
+
+    time.sleep(2)
+
 def main():
-    st.title("Company Policy Q%A Chatbot")
+    st.title("Company Policy Q&A Chatbot")
 
-    # 用户输入
-    user_input = st.text_input("Please type in your question：", key="user_input")
+    # 初始化 session_state 用于存储对话历史
+    if 'chat_history' not in st.session_state:
+        st.session_state['chat_history'] = []
 
+    # 创建一个容器用于显示对话历史
+    chat_container = st.container()
+
+    # 在对话历史容器中显示对话历史
+    with chat_container:
+        for chat in st.session_state['chat_history']:
+            st.markdown(chat, unsafe_allow_html=True)  # 使用 Markdown 格式显示对话
+
+    # 用户输入框和提交按钮放在底部
+    user_input = st.text_input("Please type in your question:", key="user_input")
     if st.button("Submit"):
         retrieved_docs = retrieve_documents(user_input)
         response = ask_gpt3(user_input, retrieved_docs)
-        display_response(response)
 
+        # 更新对话历史
+        question = f"<b>User Input/Question:</b> {user_input}<br>"
+        answer = f"<b>Chatbot Answer:</b> {response}<br>"
+        st.session_state['chat_history'].append(question)
+        st.session_state['chat_history'].append(answer)
+
+        # 逐字显示回复
+        display_response(answer)
 
 if __name__ == "__main__":
     main()
